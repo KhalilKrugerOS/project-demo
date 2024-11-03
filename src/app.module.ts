030071ade4716f1262bf11937324d41e1b645dad
khalilofy <khaliloskruger@gmail.com>
2024-11-03 10:42:09 +0100

    added payment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookingModule } from './booking/booking.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [BookingModule],
+  imports: [BookingModule, PaymentModule],
   controllers: [AppController,],
   providers: [AppService],
 })
