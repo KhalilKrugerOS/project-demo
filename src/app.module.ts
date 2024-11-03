402721c368d1a1e2b803206ccf5520a4455e64c3
khalilofy <khaliloskruger@gmail.com>
2024-11-03 10:17:42 +0100

    added booking controller&service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookingController } from './booking/booking.controller';
+import { BookingService } from './booking/booking.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BookingController],
+  providers: [AppService, BookingService],
 })
-export class AppModule {}
+export class AppModule { }
